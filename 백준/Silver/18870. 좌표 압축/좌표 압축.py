2c4e39c9c6e069bd2d5c6f445ca6512046760c62
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-08-24 17:43:55 +0900

    [Silver II] Title: 좌표 압축, Time: 1812 ms, Memory: 154796 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -4,8 +4,6 @@ input = sys.stdin.readline
 N = int(input())
 arr = list(map(int, input().split()))
 
-numDict = {}
-for i, v in enumerate(sorted(set(arr))):
-    numDict[v] = i
+numDict = { v:i for i, v in enumerate(sorted(set(arr)))}
     
 print(*[numDict[i] for i in arr], sep=" ")
