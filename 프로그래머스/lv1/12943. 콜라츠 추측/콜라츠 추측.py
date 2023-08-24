593401e48cf760595d1c9207068dca3cf2ce473a
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-08-24 11:41:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,14 +1,11 @@
 def solution(num):
-    answer = 0
-    while (answer < 500):
+    for i in range(500):
         if num == 1:
-            return answer
+            return i
         
         if num % 2 == 0:
             num //= 2
         else:
             num = num * 3 + 1
-        
-        answer += 1
-        
+            
     return -1
