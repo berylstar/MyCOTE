a89ba2bd9d8b12d4ffc8a39604830627c6363f92
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-08-24 17:07:49 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.25 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cs
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cs
@@ -1,31 +1,13 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
 public class Solution {
-    public int[] solution(int[] arr)
-    {
+    public int[] solution(int[] arr) {
         if (arr.Length == 1)
             return new int[] { -1 };
         
-        int[] answer = new int[arr.Length - 1];
-
-        int minIndex = 0;
-        for (int i = 1; i < arr.Length; i++)
-        {
-            if (arr[minIndex] > arr[i])
-                minIndex = i;
-        }
-
-        int j = 0;
-        int k = 0;
-        while (j < arr.Length)
-        {
-            if (j != minIndex)
-            {
-                answer[k] = arr[j];
-                k += 1;
-            }
-
-            j += 1;
-        }
-
-        return answer;
+        int min = arr.Min();
+        return arr.Where(num => num > min).ToArray();
     }
 }
