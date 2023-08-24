08c2890bf6e1a402e673cc603179d7a3863fb397
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-08-24 10:10:52 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.92 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        string str = n.ToString();
+        for (int i = 0; i < str.Length; i++)
+        {
+            answer += int.Parse(str[i].ToString());
+        }
+        
+        return answer;
+    }
+}
