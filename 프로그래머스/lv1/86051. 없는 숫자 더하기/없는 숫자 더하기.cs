e0dba03961baaa1cb009ebd1016cb5c21bb4d37a
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-08-24 14:53:51 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.21 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        foreach (int num in numbers)
+            answer -= num;
+        return answer;
+    }
+}
