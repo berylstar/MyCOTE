59568b04fce954c0bcd814618f524045a0c26299
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-08-24 21:21:02 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.23 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.cs
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.cs
@@ -2,12 +2,6 @@
 //     public string solution(int num) => (num % 2 == 0) ? "Even" : "Odd";
 // }
 
-// public class Solution {
-//     public string solution(int num) {
-//         string[] evenOrOdd = { "Even", "Odd" };
-//         return evenOrOdd[num % 2];
-//     }
-// }
 using System;
 public class Solution {
     public string solution(int num) => new string[] { "Even", "Odd" }[Math.Abs(num) % 2];
