bb7345b84c8c7fba5899cfef95bcd49a1889b589
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-08-24 11:38:00 +0900

    [level 1] Title: 하샤드 수, Time: 0.16 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool solution(int x) {
+        int n = x;
+        int sum = 0;
+        while (x > 0)
+        {
+            sum += x % 10;
+            x /= 10;
+        }
+        return n % sum == 0;
+    }
+}
