12911c0ec50e29c4424763534b7d096135973086
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-01 11:16:21 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,8 +1,10 @@
 def solution(k, m, score):
     apples = sorted(score, reverse=True)
     answer = 0
-    i = m - 1
-    while i < len(apples):
+    # i = m - 1
+    # while i < len(apples):
+    #     answer += apples[i] * m
+    #     i += m
+    for i in range(m-1, len(apples), m):
         answer += apples[i] * m
-        i += m
     return answer
