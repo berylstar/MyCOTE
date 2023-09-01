ec9b43c1027df08961eebd9056041016a9a67265
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-01 11:48:47 +0900

    [level 1] Title: 모의고사, Time: 1.38 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+public class Solution
+{
+    public int[] solution(int[] answers)
+    {
+        int[][] student = new int[][] { new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 1, 2, 3, 2, 4, 2, 5 }, new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } };
+        int[] score = new int[3] { 0, 0, 0 };
+
+        for (int i = 0; i < answers.Length; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (answers[i] == student[j][i % student[j].Length])
+                    score[j] += 1;
+            }
+        }
+
+        List<int> answer = new List<int>();
+        for (int i = 0; i < 3; i++)
+        {
+            if (score[i] == score.Max())
+            {
+                answer.Add(i + 1);
+            }
+        }
+        return answer.ToArray();
+    }
+}
