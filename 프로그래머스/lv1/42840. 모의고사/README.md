4298a426f81a5898cf7d86d7f3be99fe510f63d8
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-01 15:32:31 +0900

    [level 1] Title: 모의고사, Time: 2.41 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 1.38 ms
+메모리: 31.4 MB, 시간: 2.41 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.cs
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.cs
@@ -5,15 +5,14 @@ public class Solution
 {
     public int[] solution(int[] answers)
     {
-        int[][] student = new int[][] { new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 1, 2, 3, 2, 4, 2, 5 }, new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } };
+        int[][] student = new int[3][] { new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 1, 2, 3, 2, 4, 2, 5 }, new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } };
         int[] score = new int[3] { 0, 0, 0 };
 
         for (int i = 0; i < answers.Length; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                if (answers[i] == student[j][i % student[j].Length])
-                    score[j] += 1;
+                score[j] += (answers[i] == student[j][i % student[j].Length]) ? 1 : 0;
             }
         }
 
