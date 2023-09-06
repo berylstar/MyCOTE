2f86b755f72aecd29a58dfc398d427c1a7d873b8
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-06 10:36:32 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.97 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
@@ -8,31 +8,19 @@ public class Solution
         string answer = "";
         string type = "RCJATFMN";
         int[] count = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
-        Dictionary<string, int> val = new Dictionary<string, int>() { { "RT", 1 }, { "CF", 1 }, { "JM", 1 }, { "AN", 1 }, { "TR", -1 }, { "FC", -1 }, { "MJ", -1 }, { "NA", -1 } };
         Dictionary<string, int> idx = new Dictionary<string, int>() { { "RT", 0 }, { "CF", 1 }, { "JM", 2 }, { "AN", 3 }, { "TR", 4 }, { "FC", 5 }, { "MJ", 6 }, { "NA", 7 } };
 
         for (int i = 0; i < survey.Length; i++)
         {
-            Console.WriteLine($"{survey[i]} {idx[survey[i]]} {(4 - choices[i])}");
             count[idx[survey[i]]] += (4 - choices[i]);
-        }
-        
-        Console.WriteLine();
-        
-        for (int i = 0; i < 8; i++)
-        {
-            Console.WriteLine($"{i} {count[i]} {type[i]}");
         }
 
         for (int i = 0; i < 4; i++)
         {
-            if (count[i] >= count[i + 4])
-                answer += type[i];
-            else
-                answer += type[i + 4];
+            answer += (count[i] >= count[i + 4]) ? type[i] : type[i + 4];
         }
 
-        
+
         return answer;
     }
 }
