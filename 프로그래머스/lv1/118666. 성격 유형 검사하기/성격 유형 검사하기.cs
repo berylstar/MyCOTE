6147d5db6007faccb0b51d07213ae0ae3371a752
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-06 10:40:37 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.33 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cs
@@ -1,25 +1,20 @@
-using System;
-using System.Collections.Generic;
-
 public class Solution
 {
     public string solution(string[] survey, int[] choices)
     {
         string answer = "";
-        string type = "RCJATFMN";
+        string type = "RTCFJMAN";
         int[] count = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
-        Dictionary<string, int> idx = new Dictionary<string, int>() { { "RT", 0 }, { "CF", 1 }, { "JM", 2 }, { "AN", 3 }, { "TR", 4 }, { "FC", 5 }, { "MJ", 6 }, { "NA", 7 } };
 
         for (int i = 0; i < survey.Length; i++)
         {
-            count[idx[survey[i]]] += (4 - choices[i]);
+            count[type.IndexOf(survey[i][0])] += (4 - choices[i]);
         }
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 8; i += 2)
         {
-            answer += (count[i] >= count[i + 4]) ? type[i] : type[i + 4];
+            answer += (count[i] >= count[i + 1]) ? type[i] : type[i + 1];
         }
-
 
         return answer;
     }
