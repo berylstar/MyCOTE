4de1d2b4f9d18a4b52ea3afb91f866dd31484f4b
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-09 10:14:19 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,16 +1,16 @@
 def solution(id_list, report, k):
-    banned = { _id:[] for _id in id_list}
+    idxs = {v:i for i,v in enumerate(id_list)}
+    ban = {_id:[] for _id in id_list}
     
-    for rep in set(report):
-        a, b = rep.split()
-        
-        banned[b].append(id_list.index(a))
+    for repo in set(report):
+        a, b = repo.split()
+        ban[b] = ban.get(b, []) + [idxs[a]]
         
     answer = [0 for _ in range(len(id_list))]
         
-    for ban in banned.keys():
-        if len(banned[ban]) >= k:
-            for i in banned[ban]:
+    for b in ban:
+        if len(ban[b]) >= k:
+            for i in ban[b]:
                 answer[i] += 1
     
     return answer
