cf7a0804af60bd948a1c20cc2d7fab251896f847
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-09 10:00:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,15 @@
+import heapq
+def solution(operations):
+    q = []
+    for op in operations:
+        if op.startswith('I'):
+            heapq.heappush(q, int(op[2:]))
+            
+        elif len(q) > 0:        
+            if op == "D 1":
+                q.remove(max(q))
+                heapq.heapify(q)
+            else:
+                heapq.heappop(q)
+            
+    return [max(q), heapq.heappop(q)] if len(q) > 0 else [0, 0]
