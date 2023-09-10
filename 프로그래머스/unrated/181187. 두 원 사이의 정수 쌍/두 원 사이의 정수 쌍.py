273f521c01dfd877588279c6092bead4a28d0bbb
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-10 18:02:42 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 625.79 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,8 +1,3 @@
 
-def solution(r1, r2):
-    quar = 0
-    for i in range(r1):
-        quar += int((r2**2 - i**2)**0.5) - int((r1**2 - i**2 - 1)**0.5)
-    for i in range(r1, r2):
-        quar += int((r2**2 - i**2)**0.5)
-    return quar * 4
+def solution(r1, r2):    
+    return (sum(int((r2**2 - i**2)**0.5) - int((r1**2 - i**2 - 1)**0.5) for i in range(r1)) + sum(int((r2**2 - i**2)**0.5) for i in range(r1, r2))) * 4
