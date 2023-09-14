ce92f5684a7b772f65ebfe5a91fa06486a5b791a
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-14 09:22:19 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,15 +1,12 @@
 def GCD(a, b):
     [a, b] = [a, b] if a >= b else [b, a]
-    if a % b == 0:
-        return b
-    else:
-        return GCD(b, a % b)
+    return b if a % b == 0 else GCD(b, a% b)
 
 def LCM(a, b):
     return a * b // GCD(a, b)
 
 def solution(arr):
-    answer = arr[0]
-    for i in arr[1:]:
+    answer = 1
+    for i in arr:
         answer = LCM(answer, i)
     return answer
