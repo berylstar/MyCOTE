4a4ab86e5f5ea94b2e4fe1875f8bf1b9b937a560
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-15 10:41:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4237.87 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,10 +1,4 @@
 def solution(elements):
     l = len(elements)
     elements += elements
-    
-    answer = []
-    for i in range(1, l+1):
-        for j in range(l):
-            answer.append(sum(elements[j:j+i]))
-
-    return len(set(answer))
+    return len(set([sum(elements[j:j+i]) for j in range(l) for i in range(1, l+1)]))
