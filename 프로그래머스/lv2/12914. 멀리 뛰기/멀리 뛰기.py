25b38bd865569b99ec7af3493516259973a3d003
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-15 09:31:23 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.38 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,5 +1,5 @@
 def solution(n):
-    dp = [0, 1, 2] + [0 for _ in range(n)]
+    dp = [0, 1, 2] + [0 for _ in range(n-2)]
     
     for i in range(3, n+1):
         dp[i] = (dp[i-1] + dp[i-2]) % 1234567
