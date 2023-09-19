645af990eaa657d2106442b18349fcad970ff507
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-19 10:06:02 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,11 +1,11 @@
 def solution(priorities, location):
-    ans = 1
+    answer = 1
     while True:
         M = max(priorities)
         for i in range(len(priorities)):
             if priorities[i] == M:
                 if i == location:
-                    return ans
+                    return answer
                 priorities[i] = 0
-                ans += 1
+                answer += 1
                 M = max(priorities)
