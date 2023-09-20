9c7875a964f00acbce8e9a7007bff74c9d36341a
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-20 09:48:32 +0900

    [level 2] Title: 타겟 넘버, Time: 16.15 ms, Memory: 12.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,19 @@
+from collections import deque
+def solution(numbers, target):
+    N = len(numbers)
+    q = deque()
+    q.append([0, []])    # i, temp
+    answer = 0
+    
+    while q:
+        i, count = q.popleft()
+        
+        if len(count) == N:
+            if sum(count) == target:
+                answer += 1
+            continue
+        
+        q.append([i+1, count + [numbers[i]]])
+        q.append([i+1, count + [-numbers[i]]])
+        
+    return answer
