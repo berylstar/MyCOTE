f3b0bd78520ce068ecbe86d734898e4e03577f34
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-09-20 09:38:52 +0900

    [level 2] Title: 피로도, Time: 0.28 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,3 +1,4 @@
+## 1. Permutations
 from itertools import permutations
 def solution(k, dungeons):
     N = len(dungeons)
@@ -15,4 +16,41 @@ def solution(k, dungeons):
                 
         answer = max(answer, temp)
         
+    return answer
+
+## 2. DFS
+def solution(k, dungeons):
+    N = len(dungeons)
+    answer = set()
+    
+    def DFS(k, count, visited):
+        answer.add(count)
+        
+        for i in range(N):
+            if k >= dungeons[i][0] and not visited[i]:
+                DFS(k - dungeons[i][1], count + 1, [True if (visited[j] or i==j) else False for j in range(N)])
+    
+    DFS(k, 0, [False for _ in range(N)])
+    
+    return max(answer)
+
+## 3. BFS
+from collections import deque
+def solution(k, dungeons):
+    N = len(dungeons)
+    answer = 0
+    visited = [False for _ in range(N)]
+    
+    q = deque()
+    q.append([k, 0, visited])
+    
+    while q:
+        a, b, c = q.popleft()
+        answer = max(answer, b)
+        
+        for i in range(N):
+            if a >= dungeons[i][0] and not c[i]:
+                new_visited = [True if c[j] or i==j else False for j in range(N)]
+                q.append([a - dungeons[i][1], b + 1, new_visited])
+                
     return answer
