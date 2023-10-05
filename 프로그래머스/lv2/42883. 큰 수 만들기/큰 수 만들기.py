a3ec4928f5d0e01ea7383d746933c54c9719bfbe
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-10-05 10:52:52 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -2,15 +2,13 @@ def solution(number, k):
     stack = []
     
     for s in number:
-        
         while stack and k and int(stack[-1]) < int(s):
             stack.pop()
             k -= 1
         
         stack.append(s)
         
-    while stack and k:
+    for _ in range(k):
         stack.pop()
-        k -= 1
     
     return ''.join(stack)
