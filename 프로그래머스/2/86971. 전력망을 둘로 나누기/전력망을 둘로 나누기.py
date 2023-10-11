78399d1e7139da08a1e515c985a3d70b9c46729e
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-10-11 09:34:19 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.73 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,28 @@
+from collections import deque
+def solution(n, wires):
+    answers = []
+    
+    for i in range(len(wires)):
+        graph = [[] for _ in range(n+1)]        
+        for j in range(len(wires)):
+            if j == i:
+                continue
+            graph[wires[j][0]].append(wires[j][1])
+            graph[wires[j][1]].append(wires[j][0])
+            
+        visited = [False for _ in range(n+1)]
+        q = deque()
+
+        q.append(1)
+        
+        while q:
+            now = q.popleft()
+            
+            visited[now] = True
+            
+            for k in graph[now]:
+                if not visited[k]:
+                    q.append(k)
+                    
+        answers.append(abs(n-2*visited.count(True)))
+    return min(answers)
