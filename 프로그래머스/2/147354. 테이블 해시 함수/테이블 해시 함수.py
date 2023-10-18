877f7de144619e5d43fd9d2af8d2be0be1faea93
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-10-18 09:46:37 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,14 +1,4 @@
 from functools import reduce
 def solution(data, col, row_begin, row_end):
     data.sort(key=lambda x:(x[col-1], -x[0]))
-    
-    # s = []
-    # for i in range(row_begin, row_end+1):
-    #     s.append(sum(n%i for n in data[i-1]))
-    
-    s = [sum(n%i for n in data[i-1]) for i in range(row_begin, row_end + 1)]
-        
-    answer = s[0]
-    for n in s[1:]:
-        answer = answer ^ n
-    return answer
+    return reduce(lambda x,y:x^y, [sum(n%i for n in data[i-1]) for i in range(row_begin, row_end + 1)])
