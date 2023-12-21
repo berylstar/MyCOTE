72702a18edd4a09c53618061818aa4addd46c248
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-12-21 12:00:22 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -4,13 +4,13 @@ def solution(bandage, health, attacks):
     
     for attack in attacks:
         time = attack[0] - latest
-        health += time * bandage[1] + bandage[2] * (time // bandage[0])
-        health = min(health, maxHealth)
+        health = min(health + time * bandage[1] + bandage[2] * (time // bandage[0]), maxHealth)
         
         health -= attack[1]
-        latest = attack[0] + 1
         
         if health <= 0:
             return -1
+        
+        latest = attack[0] + 1
         
     return health
