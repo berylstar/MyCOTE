2c1ffb30f7488734be96ddb354d852bf5198a761
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-12-27 16:28:33 +0900

    [Bronze II] Title: 그릇, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/7567. 그릇/그릇.py
+++ 백준/Bronze/7567. 그릇/그릇.py
@@ -1,9 +1,6 @@
 dishes = input()
 answer = 10
 
-for i in range(1, len(dishes)):
-    if dishes[i - 1] == dishes[i]:
-        answer += 5
-    else:
-        answer += 10
+for i in range(1, len(dishes)):        
+    answer += 5 if dishes[i - 1] == dishes[i] else 10
 print(answer)
