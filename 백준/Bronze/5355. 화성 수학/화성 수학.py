a16728808774052f6134147d651ef58a68d2e6d3
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-12-27 15:37:18 +0900

    [Bronze II] Title: 화성 수학, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    mars = list(map(str, input().split()))
+    answer = float(mars[0])
+    for i in range(1, len(mars)):
+        if mars[i] == "#":
+            answer -= 7
+        elif mars[i] == "%":
+            answer += 5
+        elif mars[i] == "@":
+            answer *= 3
+                
+    print("%0.2f" % answer)
