976929703247ac5df55dcbcf7a01e39a3eed8d15
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-12-31 16:42:59 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 5.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -3,14 +3,14 @@ def solution(alp, cop, problems):
     max_alp = max(problems, key=lambda i:i[0])[0]
     max_cop = max(problems, key=lambda i:i[1])[1]
         
-    alp = min(alp, max_alp)
-    cop = min(cop, max_cop)
+    max_alp = max(0, max_alp - alp)
+    max_cop = max(0, max_cop - cop)
     
     dp = [[math.inf for _ in range(max_cop + 1)] for _ in range(max_alp + 1)]
-    dp[alp][cop] = 0
+    dp[0][0] = 0
     
-    for i in range(alp, max_alp + 1):
-        for j in range(cop, max_cop + 1):
+    for i in range(max_alp + 1):
+        for j in range(max_cop + 1):
             if i + 1 <= max_alp:
                 dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
                 
@@ -18,7 +18,7 @@ def solution(alp, cop, problems):
                 dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
 
             for alp_req, cop_req, alp_rwd, cop_rwd, time in problems:
-                if i >= alp_req and j >= cop_req:
+                if i >= alp_req - alp and j >= cop_req - cop:
                     next_alp = min(max_alp, i + alp_rwd)
                     next_cop = min(max_cop, j + cop_rwd)
                     dp[next_alp][next_cop] = min(dp[next_alp][next_cop], dp[i][j] + time)
