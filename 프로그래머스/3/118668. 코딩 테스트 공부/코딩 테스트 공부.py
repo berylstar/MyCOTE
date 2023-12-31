a7213069683c2b1b09af7ebe7de390c58929de01
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-12-31 16:27:52 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 6.93 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -0,0 +1,24 @@
+import math
+def solution(alp, cop, problems):
+    max_alp = max(problems, key=lambda i:i[0])[0]
+    max_cop = max(problems, key=lambda i:i[1])[1]
+        
+    alp = min(alp, max_alp)
+    cop = min(cop, max_cop)
+    
+    dp = [[math.inf for _ in range(max_cop + 1)] for _ in range(max_alp + 1)]
+    dp[alp][cop] = 0
+    
+    for i in range(alp, max_alp + 1):
+        for j in range(cop, max_cop + 1):
+            if i + 1 <= max_alp:
+                dp[i+1][j] = min (dp[i+1][j],dp[i][j] + 1)
+            if j + 1 <= max_cop:
+                dp[i][j+1] = min (dp[i][j+1],dp[i][j] + 1)
+
+            for alp_req,cop_req,alp_rwd,cop_rwd,cost in problems:
+                if i >= alp_req and j >= cop_req:
+                    next_alp,next_cop = min(max_alp,i + alp_rwd), min(max_cop,j + cop_rwd)
+                    dp[next_alp][next_cop] = min(dp[next_alp][next_cop],dp[i][j] + cost)
+
+    return dp[max_alp][max_cop]
