55d11740aabfbee7448af0700590c068d512782c
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-12-31 16:33:20 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 6.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -12,13 +12,14 @@ def solution(alp, cop, problems):
     for i in range(alp, max_alp + 1):
         for j in range(cop, max_cop + 1):
             if i + 1 <= max_alp:
-                dp[i+1][j] = min (dp[i+1][j],dp[i][j] + 1)
+                dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
+                
             if j + 1 <= max_cop:
-                dp[i][j+1] = min (dp[i][j+1],dp[i][j] + 1)
+                dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
 
-            for alp_req,cop_req,alp_rwd,cop_rwd,cost in problems:
+            for alp_req, cop_req, alp_rwd, cop_rwd, time in problems:
                 if i >= alp_req and j >= cop_req:
                     next_alp,next_cop = min(max_alp,i + alp_rwd), min(max_cop,j + cop_rwd)
-                    dp[next_alp][next_cop] = min(dp[next_alp][next_cop],dp[i][j] + cost)
+                    dp[next_alp][next_cop] = min(dp[next_alp][next_cop],dp[i][j] + time)
 
     return dp[max_alp][max_cop]
