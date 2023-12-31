d6b5717c9fc0b9c610eb94a97a891e26bd6d1eb9
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2023-12-31 16:33:59 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 6.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -19,7 +19,8 @@ def solution(alp, cop, problems):
 
             for alp_req, cop_req, alp_rwd, cop_rwd, time in problems:
                 if i >= alp_req and j >= cop_req:
-                    next_alp,next_cop = min(max_alp,i + alp_rwd), min(max_cop,j + cop_rwd)
-                    dp[next_alp][next_cop] = min(dp[next_alp][next_cop],dp[i][j] + time)
+                    next_alp = min(max_alp, i + alp_rwd)
+                    next_cop = min(max_cop, j + cop_rwd)
+                    dp[next_alp][next_cop] = min(dp[next_alp][next_cop], dp[i][j] + time)
 
     return dp[max_alp][max_cop]
