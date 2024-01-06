fe357290b8dad57a851f6c7a090a4741d2c1a8d6
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-01-06 20:33:33 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,12 +1,7 @@
 import sys
 input = sys.stdin.readline
-
-mans = []
-T = int(input())
-for i in range(T):
-    A, B = map(int, input().split())
-    mans.append((A, B, i))
-    
+T = int(input())    
+mans = [list(map(int, input().split())) for _ in range(T)]
 answer = []
 for i in range(T):
     count = 1
@@ -14,4 +9,4 @@ for i in range(T):
         if mans[i][0] < mans[j][0] and mans[i][1] < mans[j][1]:
             count += 1
     answer.append(count)
-print(*answer, sep=" ")
+print(*answer)
