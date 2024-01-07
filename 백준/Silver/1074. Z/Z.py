428bfdab804b3603bb08eb3b9ced00817f1f3e1d
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-01-07 15:23:45 +0900

    [Silver I] Title: Z, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,24 +1,19 @@
 N, r, c = map(int, input().split())
 answer = 0
 
-for _ in range(N):
+while N != 0:
     N -= 1
     eff = 2 ** N
-
-    if r < eff:
-        if c < eff:
-            quadrant = 0
-        else:
-            quadrant = 1
-            c -= eff
-    else:
-        if c < eff:
-            quadrant = 2
-        else:
-            quadrant = 3
-            c -= eff
+    quadrant = 0
     
+    if r >= eff:
+        quadrant += 2
         r -= eff
+    
+    if c >= eff:
+        quadrant += 1
+        c -= eff
 
     answer += eff * eff * quadrant
+    
 print(answer)
