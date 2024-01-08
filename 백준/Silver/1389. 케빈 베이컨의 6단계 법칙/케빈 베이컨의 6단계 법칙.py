e465561a66014f360fe2015f44f507aba6974bcc
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-01-08 19:53:35 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 68 ms, Memory: 35764 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+import math
+
+N, M = map(int, input().split())
+
+graph = [set() for _ in range(N+1)]
+for _ in range(M):
+    A, B = map(int, input().split())
+    graph[A].add(B)
+    graph[B].add(A)
+for i in range(1, N+1):
+    graph[i] = sorted(graph[i])
+    
+def CalcDistance(start):
+    q = deque()
+    q.append((start, 0))
+    
+    cost = [math.inf for _ in range(N+1)]
+    cost[0] = 0
+    
+    while q:
+        currNode, currCost = q.popleft()
+        
+        if currCost < cost[currNode]:
+            cost[currNode] = currCost
+        else:
+            continue
+        
+        for nextNode in graph[currNode]:
+            q.append((nextNode, currCost + 1))
+
+    return sum(cost)
+
+minCost = math.inf
+answer = 0
+
+for i in range(1, N+1):
+    curr = CalcDistance(i)
+
+    if curr < minCost:
+        answer = i
+        minCost = curr
+
+print(answer)
