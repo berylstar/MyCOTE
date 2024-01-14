159d5669d35413961be963be59669cc6eb2bac63
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-01-14 15:59:06 +0900

    [Gold V] Title: 적록색약, Time: 100 ms, Memory: 34156 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -0,0 +1,75 @@
+from sys import stdin
+input = stdin.readline
+
+from collections import deque
+
+d = {0:[1,0], 1:[-1,0], 2:[0,1], 3:[0,-1]}
+
+N = int(input())
+picture = [input().rstrip() for _ in range(N)]
+
+q = deque()
+
+# 1. ALL
+visited = [[False for _ in range(N)] for _ in range(N)]
+answer_all = 0
+for i in range(N):
+    for j in range(N):
+        if visited[i][j]:
+            continue
+        
+        q.append((i, j))
+        visited[i][j] = True
+        mark = picture[i][j]
+        while q:
+            x, y = q.popleft()
+            
+            for k in range(4):
+                nx = x + d[k][0]
+                ny = y + d[k][1]
+                
+                if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                    continue
+                    
+                if visited[nx][ny]:
+                    continue
+                    
+                if picture[nx][ny] == mark:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+        answer_all += 1
+
+# 2. R=G
+visited = [[False for _ in range(N)] for _ in range(N)]
+answer_rg = 0
+
+for i in range(N):
+    picture[i] = picture[i].replace('R', 'G')
+
+for i in range(N):
+    for j in range(N):
+        if visited[i][j]:
+            continue
+        
+        q.append((i, j))
+        visited[i][j] = True
+        mark = picture[i][j]
+        while q:
+            x, y = q.popleft()
+            
+            for k in range(4):
+                nx = x + d[k][0]
+                ny = y + d[k][1]
+                
+                if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                    continue
+                    
+                if visited[nx][ny]:
+                    continue
+                    
+                if picture[nx][ny] == mark:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+        answer_rg += 1
+
+print(answer_all, answer_rg)
