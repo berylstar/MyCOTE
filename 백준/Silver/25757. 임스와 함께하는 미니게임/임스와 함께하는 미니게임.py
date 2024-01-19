10c04834d2bad7ce2d83d596110c6d69fc3d2713
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-01-19 13:24:44 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 84 ms, Memory: 42388 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -4,5 +4,4 @@ input = stdin.readline
 games = {'Y':1, 'F':2, 'O':3}
 
 N, game = input().split()
-mans = set(input().rstrip() for _ in range(int(N)))
-print(len(mans)//games[game])
+print(len(set(input().rstrip() for _ in range(int(N))))//games[game])
