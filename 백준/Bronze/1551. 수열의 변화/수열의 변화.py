18d0657d2edeb2fbb09e4c5877d55701382cd286
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-01-30 16:41:35 +0900

    [Bronze I] Title: 수열의 변화, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1551. 수열의 변화/수열의 변화.py
+++ 백준/Bronze/1551. 수열의 변화/수열의 변화.py
@@ -5,7 +5,8 @@ N, K = map(int, input().split())
 numbers = list(map(int, input().split(',')))
 
 for _ in range(K):
-    for i in range(len(numbers) - 1):
+    for i in range(N - 1):
         numbers[i] = numbers[i+1] - numbers[i]
     numbers.pop()
+    N -= 1
 print(*numbers, sep=',')
