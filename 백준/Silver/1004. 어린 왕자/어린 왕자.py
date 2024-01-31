712c03c15c5d43fb230c7a9e1a0a91efea1962d9
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-01-31 21:42:44 +0900

    [Silver III] Title: 어린 왕자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -0,0 +1,18 @@
+from sys import stdin
+input = stdin.readline
+
+T = int(input())
+
+def CheckInPath(x, y, a, b, r):
+    return (a - x) ** 2 + (b - y) ** 2 <= r ** 2
+
+for _ in range(T):
+
+    x1, y1, x2, y2 = map(int, input().split())
+    ret = 0
+
+    for _ in range(int(input())):
+        a, b, r = map(int, input().split())
+        ret += int(CheckInPath(x1, y1, a, b, r) ^ CheckInPath(x2, y2, a, b, r))
+
+    print(ret)
