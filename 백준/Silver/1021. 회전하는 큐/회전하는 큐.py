b6d3c531058df9bd1c54b0f82b7bbfdae4b7a2e8
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-01-31 21:57:45 +0900

    [Silver III] Title: 회전하는 큐, Time: 68 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -0,0 +1,27 @@
+from sys import stdin
+input = stdin.readline
+
+from collections import deque
+
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+q = deque([i for i in range(1, N+1)])
+
+answer = 0
+for num in numbers:
+    while True:
+        
+        if q[0] == num:
+            q.popleft()
+            break
+        else:
+            if q.index(num) < len(q) / 2:
+                while q[0] != num:
+                    q.append(q.popleft())
+                    answer += 1
+            else:
+                while q[0] != num:
+                    q.appendleft(q.pop())
+                    answer += 1
+
+print(answer)
