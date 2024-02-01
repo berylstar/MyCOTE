dcbc90ef5956841e9b71b94c312066ccc35a0b37
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-01 15:23:58 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 13260 ms, Memory: 50104 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
@@ -7,11 +7,10 @@ for _ in range(int(input())):
     max_val = 0
     max_key = 0 
     for i in range(1, len(line)):
-        num = int(line[i])
-        d[num] = d.get(num, 0) + 1
-        if d[num] > max_val:
-            max_val = d[num]
-            max_key = num
+        d[line[i]] = d.get(line[i], 0) + 1
+        if d[line[i]] > max_val:
+            max_val = d[line[i]]
+            max_key = line[i]
 
     if max_val > line[0] / 2:
         print(max_key)
