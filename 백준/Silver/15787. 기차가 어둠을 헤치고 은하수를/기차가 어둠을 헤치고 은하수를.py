9f1e45292c154151b7e48f579cb1ec1ef4df02a5
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-02 15:07:18 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 176 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
@@ -2,18 +2,18 @@ from sys import stdin
 input = stdin.readline
 
 N, M = map(int, input().split())
-train = [['0' for _ in range(20)] for _ in range(N)]
+trains = [0 for _ in range(N)]
 
 for _ in range(M):
     order = list(map(lambda x:int(x)-1, input().split()))
     
     if order[0] == 0:
-        train[order[1]][order[2]] = '1'
+        trains[order[1]] = trains[order[1]] | (1 << order[2])
     elif order[0] == 1:
-        train[order[1]][order[2]] = '0'
+        trains[order[1]] = trains[order[1]] & ~(1 << order[2])
     elif order[0] == 2:
-        train[order[1]] = ['0'] + train[order[1]][:19]
+        trains[order[1]] = (trains[order[1]] << 1) & ~(1 << 20)
     else:
-        train[order[1]] = train[order[1]][1:20] + ['0']
+        trains[order[1]] = trains[order[1]] >> 1
         
-print(len(set([''.join(i) for i in train])))
+print(len(set(trains)))
