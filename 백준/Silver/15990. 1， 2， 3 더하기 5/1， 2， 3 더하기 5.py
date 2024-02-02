f314b9b80ceeaae67856971b9e4e04a8761bae83
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-02 16:20:26 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 164 ms, Memory: 51356 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -0,0 +1,16 @@
+from sys import stdin
+input = stdin.readline
+
+dp = [[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 1]]
+
+for i in range(4, 100001):
+    now = [
+        (dp[i-1][1] + dp[i-1][2]) % 1000000009,
+        (dp[i-2][0] + dp[i-2][2]) % 1000000009,
+        (dp[i-3][0] + dp[i-3][1]) % 1000000009
+    ]
+    dp.append(now)
+
+for _ in range(int(input())):
+    n = int(input())
+    print(sum(dp[n]) % 1000000009)
