fdf8b1ef728cc9f02eeaaa1e32b9cb3118c1cbe3
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-04 15:45:42 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -2,5 +2,5 @@ from itertools import permutations
 
 N, M = map(int, input().split())
 
-for permutation in permutations(range(1, N+1), M):
-    print(*list(permutation))
+for p in permutations(range(1, N+1), M):
+    print(*p)
