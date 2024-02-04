bd6ba1936300bc7cdd19cda6b06d0d135ac57684
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-04 16:26:16 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,14 +1,6 @@
+from itertools import combinations_with_replacement
+
 N, M = map(int, input().split())
 
-def DFS(c):
-    if len(ret) == M:
-        print(*ret)
-    else:
-        for k in range(c, N+1):
-            ret.append(k)
-            DFS(k)
-            ret.pop()
-
-ret = []
-
-DFS(1)
+for c in combinations_with_replacement(range(1, N+1), M):
+    print(*c)
