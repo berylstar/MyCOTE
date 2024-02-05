d03ed00084b0f85e8990438c6282f3af29bee0fd
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-05 15:38:10 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,16 +1,15 @@
 from sys import stdin
 input = stdin.readline
 
-def Prime(num):
+def IsPrime(num):
     for k in range(2, int(num**(0.5)) + 1):
         if num % k == 0:
             return False
     return True
 
 for _ in range(int(input())):
-    num = int(input())
-    half = num // 2
-    for i in range(half):
-        if Prime(half - i) and Prime(half + i):
-            print(half - i, half + i)
+    num = int(input()) // 2
+    for i in range(num):
+        if IsPrime(num - i) and IsPrime(num + i):
+            print(num - i, num + i)
             break
