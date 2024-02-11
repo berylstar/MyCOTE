439569341365d67f48e0d05389c35c7b9450a8b7
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-11 17:39:23 +0900

    [Silver I] Title: 회의실 배정, Time: 272 ms, Memory: 59852 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,14 @@
+from sys import stdin
+input = stdin.readline
+
+N = int(input())
+rooms = sorted([list(map(int, input().split())) for _ in range(N)], key=lambda x:(x[1], x[0]))
+
+answer = 0
+curr = -1
+for start, end in rooms:
+    if start < curr:
+        continue
+    answer += 1
+    curr = end
+print(answer)
