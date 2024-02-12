8366c6e7c5813015a198f8745a280d8f749335cf
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-12 15:01:34 +0900

    [Silver I] Title: 스티커, Time: 920 ms, Memory: 47740 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -3,17 +3,15 @@ input = stdin.readline
 
 for _ in range(int(input())):
     N = int(input())
-    stickers = [list(map(int, input().split())) for _ in range(2)]
-    dp = [[0 for _ in range(N)] for _ in range(2)]
-    dp[0][0] = stickers[0][0]
-    dp[1][0] = stickers[1][0]
+    dp = [list(map(int, input().split())) for _ in range(2)]
     
     if N > 1:
-        dp[0][1] = dp[1][0] + stickers[0][1]
-        dp[1][1] = dp[0][0] + stickers[1][1]
+        dp[0][1] += dp[1][0]
+        dp[1][1] += dp[0][0]
     
     for j in range(2, N):
-        dp[0][j] = max(dp[1][j-1], dp[1][j-2], dp[0][j-2]) + stickers[0][j]
-        dp[1][j] = max(dp[0][j-1], dp[1][j-2], dp[0][j-2]) + stickers[1][j]
+        preMax = max(dp[0][j-2], dp[1][j-2])
+        dp[0][j] += max(dp[1][j-1], preMax)
+        dp[1][j] += max(dp[0][j-1], preMax)
 
     print(max(dp[0][-1], dp[1][-1]))
