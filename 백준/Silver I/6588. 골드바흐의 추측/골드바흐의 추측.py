48d66751b2e21f7bcbbd19451bf551e5e778b5c1
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-16 12:06:14 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 2440 ms, Memory: 39368 KB -BaekjoonHub

--- 백준/Silver I/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver I/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -11,14 +11,14 @@ for i in range(2, int(1000001 ** 0.5) + 1):
         sosu[j] = False
 
 while True:
-    buff = int(input())
+    N = int(input())
 
-    if buff == 0:
+    if N == 0:
         break
 
-    for i in range(3, buff // 2 + 1, 2):
-        if sosu[i] and sosu[buff - i]:
-            print(f"{buff} = {i} + {buff - i}")
+    for i in range(3, N // 2 + 1, 2):
+        if sosu[i] and sosu[N - i]:
+            print(f"{N} = {i} + {N - i}")
             break
     else:
         print("Goldbach's conjecture is wrong.")
