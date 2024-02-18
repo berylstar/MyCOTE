e3b5dc44ced01908c4620982ea427a1944f3e918
Minsang Kim <98025769+berylstar@users.noreply.github.com>
2024-02-18 16:25:45 +0900

    [Gold V] Title: 치킨 배달, Time: 664 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,14 +1,13 @@
 from sys import stdin
 input = stdin.readline
 
-from collections import deque
 from itertools import combinations
 
 N, M = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
-chickens = deque()
-houses = deque()
+chickens = []
+houses = []
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 1:
@@ -22,8 +21,7 @@ for comb in combinations(range(len(chickens)), M):
     for house in houses:
         curr = 2 * N
         for i in comb:
-            cx, cy = chickens[i][0], chickens[i][1]
-            curr = min(curr, abs(cx - house[0]) + abs(cy - house[1]))
+            curr = min(curr, abs(chickens[i][0] - house[0]) + abs(chickens[i][1] - house[1]))
         temp += curr
 
     answer = min(temp, answer)
